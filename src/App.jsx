167d6e69abade926e58a796aee755adebc4d6ea3
SidakVats <sidakvats6499@gmail.com>
2024-04-17 12:15:54 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Hero from "./components/Hero"
+import Demo from "./components/Demo"
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <div className="main">
+          <div className="gradient"/>
+        </div>
+          <div className="app">
+            <Hero/>
+            <Demo/>
+          </div>
+      </main>
+    </>
+  )
+}
+
+export default App
